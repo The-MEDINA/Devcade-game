176ded440fb6faba749e12fd9c07f6f45023739d
the_medina_ <147880142+The-MEDINA@users.noreply.github.com>
2024-10-11 14:43:16 -0400

    Revert "Made timer usable (and very bad)"
    This reverts commit 0d7c53d0c0be57d494ad195565a9502a9ae540b5.

--- DevcadeGame/QuickDraw1P.cs
+++ DevcadeGame/QuickDraw1P.cs
@@ -12,9 +12,9 @@ namespace WildWestShootout
         SpriteBatch _spriteBatch;
         SpriteFont _font;
         Texture2D player1Sprite;
-        TimeSpan timeMiddleMan = new TimeSpan();
+        TimeSpan test = new TimeSpan();
         public static ContentManager _content;
-        float[] convertedTimer = {0,0};
+        float timer = 0;
         //thank f*ck i got this to work. Thanks Ella :>
         //starts the QuickDraw(1P) gamemode.
         public QuickDraw1P(SpriteBatch spriteBatch, SpriteFont font, ContentManager Content)
@@ -32,20 +32,13 @@ namespace WildWestShootout
         //drawing out the game here.
         public void DrawThis()
         {
-            _spriteBatch.DrawString(_font, $"Quick Draw (1P) gamemode.", new Vector2(0, 0), Color.Black);
+            _spriteBatch.DrawString(_font, $"Quick Draw (1P) gamemode.\n{timer}", new Vector2(0, 0), Color.Black);
             _spriteBatch.Draw(player1Sprite, new Vector2(32,490), Color.White);	
         }
-        //Here's where the game stuff is gonna happen (I say gonna cause as of writing this it doesn't do much)
         public void UpdateThis(GameTime _gameTime)
         {
-            //- - - - - - - - - - - - - - -
-            //Yes, I'm aware this is probably not the best way to do this. Or.. a good way to do this.
-            //gameTime is giving me so many issues though, so this is the best I could do make it usable. 
-            timeMiddleMan = timeMiddleMan+_gameTime.ElapsedGameTime;
-            convertedTimer[0] = timeMiddleMan.Seconds;
-            convertedTimer[1] = timeMiddleMan.Milliseconds;
-            //- - - - - - - - - - - - - - -
-            System.Console.WriteLine($"{convertedTimer[0]}, {convertedTimer[1]}");
+            test = (test+_gameTime.ElapsedGameTime)/60;
+            System.Console.WriteLine("quickdraw1 time"+test);
             DrawThis();
         }
     }
